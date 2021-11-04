7d45a176713bb26e811d5749e79b554292474357
Varshini98 <varshininaikk1998@gmail.com>
2021-11-04 10:42:05 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,79 @@
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+	
+import cv2
+import mediapipe as mp
+import time
+ 
+cap = cv2.VideoCapture(1)
+ 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+ 
+pTime = 0
+cTime = 0
+ 
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+ 
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                # if id == 4:
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+ 
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+ 
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+ 
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 255), 3)
+ 
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
